750abdf740fe7f0f3b02d98bccb2ef7c0a2b7a92
guanjun <gaunjunace@foxmail.com>
2018-06-21 10:16:59 +0800

    dp print path solved

--- 0003.cpp
+++ 0003.cpp
@@ -5,27 +5,36 @@ File Name     :0003.cpp
 ************************************************ */
 #include <bits/stdc++.h>
 using namespace std;
-vector<vector<int> > dp(1010, vector<int>(1010));
-vector<vector<int> > pre(1010, vector<int>(1010));
 string s, t;
-void dfs(int x, int y, int len) {
-	if (len == 0) {
-		cout << endl;
+string ans;
+int dp[88][88];
+int pre[88][88];
+map<string, bool>mp[88][88];
+set<string> se;
+
+void dfs(int x, int y, string ans) {
+	if (mp[x][y][ans]) return;
+	if (x == 0 || y == 0) {
+		se.insert(ans);
 		return ;
 	}
-	if (pre[x][y] == 0) {
-		dfs(x-1, y-1, len-1);
-		cout << s[x-1];
+	if (s[x-1] == t[y-1]) {
+		ans = s[x-1] + ans;
+		dfs(x-1, y-1, ans);
 	}
-	else if (pre[x][y] == 1) {
-		dfs(x-1, y, len);
-		cout << s[x-1];
+	else if (dp[x-1][y] > dp[x][y-1]) {
+		dfs(x-1, y, ans);
 	}
-	else if (pre[x][y] == -1){
-		dfs(x, y-1, len);
-		cout << t[y-1];
+	else if (dp[x][y-1] > dp[x-1][y]) {
+		dfs(x, y-1, ans);
 	}
+	else {
+		dfs(x-1, y, ans);
+		dfs(x, y-1, ans);
+	}
+	mp[x][y][ans] = true;
 }
+
 int main() {
 	int T;
 	cin >> T;
@@ -33,11 +42,13 @@ int main() {
 		cin >> s >> t;
 		int n = s.size();
 		int m = t.size();
-		for (int i = 0; i <= n; ++i) pre[i][0] = 1; // from top
-		for (int j = 0; j <= m; ++j) pre[0][j] = -1; // from left
-
+		for (int i = 0; i <= n; ++i) pre[i][0] = 1, mp[i][0].clear(); // from top
+		for (int j = 0; j <= m; ++j) pre[0][j] = -1, mp[0][j].clear(); // from left
+		memset(dp, 0, sizeof dp);
+		se.clear();
 		for (int i = 1; i <= n; ++i) {
 			for (int j = 1; j <= m; ++j) {
+				mp[i][j].clear();
 				if (s[i-1] == t[j-1]) {
 					dp[i][j] = dp[i-1][j-1]+1;
 					pre[i][j] = 0;
@@ -54,8 +65,12 @@ int main() {
 			}
 			//cout << endl;
 		}
-		dfs(n, m, dp[n][m]);
-		//if (T) cout << endl;
+		string ans = "";
+		dfs(n, m, ans);
+		for (auto x : se) {
+			cout << x << endl;
+		}
+		//if (T > 0) cout << endl;
 	}
 	return 0;
 }
